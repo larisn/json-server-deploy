a211fe6e5b6f977ef53be6ab8c8b1d0be7e865ec
Larissa <contatolarisn@gmail.com>
2023-10-25 17:41:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const cors = require("cors");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
@@ -9,9 +8,3 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
-
-server.use(cors({
-  origin: ["http://localhost:5173", "https://json-server-deploy.glitch.me"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-}));
